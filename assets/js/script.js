b850ab59f66fed19a0fd1a61ff9ed4e59d3d793c
KWensmann <kurtwensmann@gmail.com>
2023-10-15 11:01:58 -0500

    splash page

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const highScoreBtn = document.getElementById('scoreboard');   //$("#exampleModal")
+const myModal = document.getElementsByClassName(".modal")
+
+
+
+highScoreBtn.addEventListener('click', function(event) {
+  console.log(event.target)
+
+  myInput.focus()
+})
