ccd6d01f5b440ddafbc63b65f0676bd3ba27917a
KWensmann <kurtwensmann@gmail.com>
2023-10-16 10:43:00 -0500

    pull commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
-const highScoreBtn = document.getElementById('scoreboard');   //$("#exampleModal")
-const myModal = document.getElementsByClassName(".modal")
-
-
-
-highScoreBtn.addEventListener('click', function(event) {
+$("#scoreboard").on("click", (event) => {
+  //if data-show is base the css on display block or display none
+  var openModal = $('#modalBtn')
   console.log(event.target)
+  if (event.target === openModal) {
+    $("main").animate({
+      opacity: 0.2  // Set to desired final opacity
+    }, 1000);  // Duration in milliseconds
+  
+    $('#modal').fadeIn(2000)
+    $('#modal').css({"display": "block", "border": "2px solid black", "background-color": "blue"})
+  }
+})
 
+
+
+
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
