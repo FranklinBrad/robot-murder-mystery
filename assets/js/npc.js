e7525b07d336f332c08356fc05be9333836cd29f
KWensmann <kurtwensmann@gmail.com>
2023-10-18 12:19:08 -0500

    testing

--- assets/js/npc.js
+++ assets/js/npc.js
@@ -353,7 +353,7 @@ function winGame(){
   console.log(playerName)
   playerScore = roboNPC.length;
   saveHighScore();
-  init()
+
   var snd = new Audio("./assets/sound/Victory.mp3"); //wav is also supported
   snd.play(); //plays the sound
   // fadeIn over 3 seconds
